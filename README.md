899b7bd78d3fb94072f69e0fbdfab7438b1c5034
Matteo <mateus.sakata@gmail.com>
2022-01-08 21:35:18 -0600

    Add Spotless to Robot Project
    Added Spotless in accordance to this tutorial:
    https://docs.wpilib.org/en/stable/docs/software/advanced-gradlerio/code-formatting.html?highlight=ci#spotless

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 The repository for FRC 6901's 2022 Robot Code.
 
 There are also issues in this repository to track components outside of programming to test the beta Github Projects.
+
+## Formatter Usage
+
+This project uses spotless formatting for code. To automatically format code with spotless, run:
+`./gradlew spotlessApply`
--- src/test/java/frc/robot/TestSystemTest.java
+++ src/test/java/frc/robot/TestSystemTest.java
@@ -1,15 +1,14 @@
 package frc.robot;
 
 import static org.junit.Assert.*;
+
 import org.junit.*;
 
-/**
- * Class that tests the system test
- */
+/** Class that tests the system test */
 public class TestSystemTest {
-    @Test
-    public void test() {
-        // assert statements
-        Assert.assertEquals(0, 0);
-    }
+  @Test
+  public void test() {
+    // assert statements
+    Assert.assertEquals(0, 0);
+  }
 }
