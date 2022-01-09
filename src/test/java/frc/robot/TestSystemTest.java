56a7ce46c97f8d86658be477dbaae5e7e9e15b23
Matteo <mateus.sakata@gmail.com>
2022-01-08 21:33:05 -0600

    Add Unit Testing to project
    Added unit testing to build.gradle file with this:
    https://docs.wpilib.org/en/stable/docs/software/wpilib-tools/robot-simulation/unit-testing.html?highlight=tests#running-tests
    
    Used Team 254's Test System Test:
    https://github.com/Team254/FRC-2020-Public/blob/master/src/test/java/com/team254/frc2020/TestSystemTest.java

---
+++ src/test/java/frc/robot/TestSystemTest.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+/**
+ * Class that tests the system test
+ */
+public class TestSystemTest {
+    @Test
+    public void test() {
+        // assert statements
+        Assert.assertEquals(0, 0);
+    }
+}
