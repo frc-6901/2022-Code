0566045b0da55a386afa154bdaee270f3a99d02e
Matteo Kimura <mateus.sakata@gmail.com>
2022-03-18 22:52:36 -0500

    Make two ball auto with timer (#34)

--- src/main/java/frc/robot/commands/TwoBallAuto.java
+++ src/main/java/frc/robot/commands/TwoBallAuto.java
@@ -4,12 +4,7 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -19,7 +14,6 @@ import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Indexer;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
-import java.util.ArrayList;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
@@ -27,34 +21,33 @@ import java.util.ArrayList;
 public class TwoBallAuto extends SequentialCommandGroup {
   /** Creates a new TwoBallAuto. */
   public TwoBallAuto(Drivetrain drive, Shooter shooter, Indexer indexer, Intake intake) {
-    var forwardTrajectory =
-        TrajectoryGenerator.generateTrajectory(
-            new Pose2d(0, 0, new Rotation2d(0)),
-            new ArrayList<Translation2d>(),
-            new Pose2d(Units.feetToMeters(7.75), 0, new Rotation2d(0)),
-            drive.getTrajectoryConfig());
-
-    var reverseConfig =
-        new TrajectoryConfig(DrivetrainConstants.kAutoMaxSpeed, DrivetrainConstants.kAutoMaxAccel);
-    reverseConfig.setReversed(true);
-    var backwardTrajectory =
-        TrajectoryGenerator.generateTrajectory(
-            new Pose2d(Units.feetToMeters(7.75), 0, new Rotation2d(0)),
-            new ArrayList<Translation2d>(),
-            new Pose2d(1, 0, new Rotation2d(0)),
-            reverseConfig);
-
-    var fullTrajectory = forwardTrajectory.concatenate(backwardTrajectory);
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
+        new ShootCommand(ShooterConstants.kShooterFenderRPM, 4.0, shooter, indexer),
         new ParallelCommandGroup(
             new RunCommand(
                 () -> {
                   intake.intakeBalls();
                 },
                 intake),
-            drive.getTrajectoryFollowerCommand(fullTrajectory)),
+            new RunCommand(
+                    () -> {
+                      drive.drive(0.5, 0.0);
+                    },
+                    drive)
+                .withTimeout(DrivetrainConstants.kAutoTime)),
+        new InstantCommand(
+            () -> {
+              intake.retractIntake();
+            },
+            intake),
+        new RunCommand(
+                () -> {
+                  drive.drive(-0.5, 0.0);
+                },
+                drive)
+            .withTimeout(DrivetrainConstants.kAutoTime),
         new ShootCommand(ShooterConstants.kShooterFenderRPM, 4.0, shooter, indexer));
   }
 }
